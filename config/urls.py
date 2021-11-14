8d0464f3060ddeefffe6e0972cd469c7549e5086
ishikawa16 <shin.0116@keio.jp>
2021-11-14 23:42:41 +0900

    update: add path to urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('accounts/', include('accounts.urls')),
+    path('players/', include('players.urls')),
 ]
