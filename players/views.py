afbf775863599ae169ffc4c8979000f3bdfa7ce0
ishikawa16 <shin.0116@keio.jp>
2021-11-15 00:06:56 +0900

    add: player list/detail views

--- players/views.py
+++ players/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from accounts.models import CustomUser
 
-# Create your views here.
+
+class PlayerListView(ListView):
+    model = CustomUser
+    template_name = 'players/player_list.html'
+    context_object_name = 'player_list'
+
+
+class PlayerDetailView(DetailView):
+    model = CustomUser
+    template_name = 'players/player_detail.html'
+    context_object_name = 'player'
