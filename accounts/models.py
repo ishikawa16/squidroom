d564e4b916214ed4668f100314c1cfe5354a2021
ishikawa16 <shin.0116@keio.jp>
2021-11-16 02:37:30 +0900

    update: add help_text to 'code'

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     rate = models.IntegerField(default=1500, verbose_name='rate')
-    code = models.CharField(max_length=20, validators=[RegexValidator(r'^\d{4}-\d{4}-\d{4}$', 'Your code was inappropriate.')], verbose_name='friend code (xxxx-xxxx-xxxx)')
+    code = models.CharField(help_text='Fill in as "xxxx-xxxx-xxxx."', max_length=20, validators=[RegexValidator(r'^\d{4}-\d{4}-\d{4}$', 'Your code was inappropriate.')], verbose_name='friend code')
