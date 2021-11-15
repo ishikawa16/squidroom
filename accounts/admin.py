6304404e12c30182686d5563f00f0d416ff6569c
ishikawa16 <shin.0116@keio.jp>
2021-11-16 02:24:30 +0900

    update: add 'code' to custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,8 +3,8 @@ from .models import CustomUser
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'rate')
-    list_display_links = ('id', 'username', 'rate')
+    list_display = ('id', 'username', 'rate', 'code')
+    list_display_links = ('id', 'username', 'rate', 'code')
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class CustomUser(AbstractUser):
     rate = models.IntegerField(default=1500, verbose_name='rate')
+    code = models.CharField(max_length=20, validators=[RegexValidator(r'^\d{4}-\d{4}-\d{4}$', 'Your code was inappropriate.')], verbose_name='friend code (xxxx-xxxx-xxxx)')
