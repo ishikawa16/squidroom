07544971653ff720af752865f4e51de4a6bf6bfe
ishikawa16 <shin.0116@keio.jp>
2021-11-21 18:31:01 +0900

    add: define standby view

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Room
 
-# Create your views here.
+
+class StandbyView(TemplateView):
+    template_name = 'rooms/standby.html'
+
+    def get(self, request, *args, **kargs):
+        Room.objects.create(player=request.user)
+        return super().get(request, *args, **kargs)
