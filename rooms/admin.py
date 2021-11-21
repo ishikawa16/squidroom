dc16ca19c472959c9376eea352743867d25a4927
ishikawa16 <shin.0116@keio.jp>
2021-11-21 18:30:25 +0900

    update: add room model to admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Room
 
-# Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'player', 'posted_time')
+    list_display_links = ('id', 'player', 'posted_time')
+
+
+admin.site.register(Room, RoomAdmin)
