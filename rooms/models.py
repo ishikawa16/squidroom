6944ce209a33b0201adf3c9eed2a79aacdae3728
ishikawa16 <shin.0116@keio.jp>
2021-11-21 18:30:04 +0900

    add: define room model

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class Room(models.Model):
+    player = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    posted_time = models.DateTimeField(auto_now_add=True)
