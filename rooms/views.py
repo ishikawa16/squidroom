d0a7cfc87a3d63275418b5d0b0496409dccf4b43
ishikawa16 <shin.0116@keio.jp>
2021-11-27 18:34:20 +0900

    add: cancel page

--- rooms/views.py
+++ rooms/views.py
@@ -8,3 +8,11 @@ class StandbyView(TemplateView):
     def get(self, request, *args, **kargs):
         Room.objects.create(player=request.user)
         return super().get(request, *args, **kargs)
+
+
+class CancelView(TemplateView):
+    template_name = 'rooms/cancel.html'
+
+    def get(self, request, *args, **kargs):
+        Room.objects.filter(player=request.user).delete()
+        return super().get(request, *args, **kargs)
