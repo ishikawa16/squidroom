8e2239aca91d22bf40c93644d881a3c16541a230
ishikawa16 <shin.0116@keio.jp>
2021-11-28 17:08:18 +0900

    update: battle framework

--- rooms/views.py
+++ rooms/views.py
@@ -5,14 +5,27 @@ from .models import Room
 class BattleView(TemplateView):
     template_name = 'rooms/battle.html'
 
-    def get(self, request, *args, **kargs):
-        Room.objects.create(player=request.user)
-        return super().get(request, *args, **kargs)
+    def get_context_data(self, **kwargs):
+        try:
+            room = Room.objects.get(player1=self.request.user)
+
+        except Room.DoesNotExist:
+            try:
+                room = Room.objects.exclude(player1=self.request.user).earliest('posted_on')
+                room.player2 = self.request.user
+                room.save()
+            except Room.DoesNotExist:
+                room = Room.objects.create(player1=self.request.user)
+
+        finally:
+            context = super().get_context_data(**kwargs)
+            context['room'] = room
+            return context
 
 
 class CancelView(TemplateView):
     template_name = 'rooms/cancel.html'
 
     def get(self, request, *args, **kargs):
-        Room.objects.filter(player=request.user).delete()
+        Room.objects.filter(player1=request.user).delete()
         return super().get(request, *args, **kargs)
