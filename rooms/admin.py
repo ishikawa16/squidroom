b5a09fa51f2d0440afbfb33e9da5164a3c07ad08
ishikawa16 <shin.0116@keio.jp>
2021-11-28 17:05:38 +0900

    add: new fields 'player2' and 'result' to Room

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,8 +3,8 @@ from .models import Room
 
 
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ('id', 'player', 'posted_on')
-    list_display_links = ('id', 'player', 'posted_on')
+    list_display = ('id', 'player1', 'player2', 'posted_on', 'result')
+    list_display_links = ('id', 'player1', 'player2', 'posted_on', 'result')
 
 
 admin.site.register(Room, RoomAdmin)
--- rooms/models.py
+++ rooms/models.py
@@ -3,5 +3,7 @@ from accounts.models import CustomUser
 
 
 class Room(models.Model):
-    player = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    posted_on = models.DateTimeField(auto_now_add=True)
+    player1 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='player1', verbose_name='player1')
+    player2 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='player2', verbose_name='player2', blank=True, null=True)
+    posted_on = models.DateTimeField(auto_now_add=True, verbose_name='posted_on')
+    result = models.IntegerField(default=0, verbose_name='result')
