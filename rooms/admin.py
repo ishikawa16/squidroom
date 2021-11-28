711866e4c3195cd7d120b536a8954cc5de70dccc
ishikawa16 <shin.0116@keio.jp>
2021-11-28 18:52:37 +0900

    change: 'result' to 'winner' in Room

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,8 +3,8 @@ from .models import Room
 
 
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ('id', 'player1', 'player2', 'posted_on', 'result')
-    list_display_links = ('id', 'player1', 'player2', 'posted_on', 'result')
+    list_display = ('id', 'player1', 'player2', 'posted_on', 'winner')
+    list_display_links = ('id', 'player1', 'player2', 'posted_on', 'winner')
 
 
 admin.site.register(Room, RoomAdmin)
--- rooms/models.py
+++ rooms/models.py
@@ -6,4 +6,4 @@ class Room(models.Model):
     player1 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='player1', verbose_name='player1')
     player2 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='player2', verbose_name='player2', blank=True, null=True)
     posted_on = models.DateTimeField(auto_now_add=True, verbose_name='posted_on')
-    result = models.IntegerField(default=0, verbose_name='result')
+    winner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='winner', verbose_name='winner',  blank=True, null=True)
